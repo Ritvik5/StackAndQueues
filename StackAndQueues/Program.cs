2f53169420a41c2a8ae1fe24235148e842a4bef8
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-18 14:11:49 +0530

    [Ritvik]Add: Using push adding elements in Stack

--- StackAndQueues/Program.cs
+++ StackAndQueues/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack And Queues!!!");
+
+            StackExecution obj = new StackExecution();
+
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+
+            obj.Display();
+            
         }
     }
 }
---
+++ StackAndQueues/StackExecution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    public class StackExecution
+    {
+        public Node top;
+
+        public StackExecution()
+        {
+            top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = top;
+            top = newNode;
+        }
+
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty!!!");
+            }
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine(" | "+temp.data+" | ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
