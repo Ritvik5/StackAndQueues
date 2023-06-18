85c15fca54e3cb1ec2f11ad7eeda9abea7a1f667
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-18 13:54:15 +0530

    [Ritvik]Add: Welcome Messagegit add .

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queues!!!");
+        }
+    }
+}
